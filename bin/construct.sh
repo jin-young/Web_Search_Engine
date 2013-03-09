0b21026b098d6749b1af362e44f0b5bfce1ef87f
oveis <jinil@nyu.edu>
2013-03-09 11:08:09 -0500

    modify shell script path

--- bin/construct.sh
+++ bin/construct.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -cp ../src edu.nyu.cs.cs2580.SearchEngine \
---mode=index --options=../conf/engine.conf
+java -cp src edu.nyu.cs.cs2580.SearchEngine \
+--mode=index --options=conf/engine.conf
 exit 0
--- bin/start_index.sh
+++ bin/start_index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -cp ../src -Xmx256m edu.nyu.cs.cs2580.SearchEngine \
--mode=index --port=25804 --options=../conf/engine.conf
+java -cp src -Xmx256m edu.nyu.cs.cs2580.SearchEngine \
+-mode=index --port=25804 --options=conf/engine.conf
 exit 0
