15b1157ab614d6ded6f648d68908075c82c7ab2d
soo <ss6321@nyu.edu>
2013-04-09 01:28:56 -0400

    spearman

--- src/edu/nyu/cs/cs2580/Spearman.java
+++ src/edu/nyu/cs/cs2580/Spearman.java
@@ -10,9 +10,23 @@ import java.util.Map;
 
 public class Spearman {
 
-	public static Integer calculate(Map<String, Integer> numvew, Map<String, Document> pagerank){
-		List<Document> rankList = new LinkedList<Document>(pagerank.values());
-		Collections.sort(rankList, new Comparator<Document>() {
+	public static Float calculate(List<Document> sortednumview, List<Document> sortedpagerank){
+		int index=0;
+		float result=0;
+		float temp=0;
+		String doc=sortednumview.get(index).getTitle();
+		while(doc != null){
+			temp=temp+(float) Math.exp(sortedpagerank.indexOf(doc)-sortednumview.indexOf(doc));
+			index++;	
+			doc=sortednumview.get(index).getTitle();
+		}
+		result=(float) (1-6*temp/(sortednumview.size()*Math.exp(sortednumview.size()-1)));
+		return result;
+	}
+	
+	public static List<Document> sorting(Map<String, Document> temp){
+		List<Document> sortedList = new LinkedList<Document>(temp.values());
+		Collections.sort(sortedList, new Comparator<Document>() {
 			@Override
             public int compare(Document o1, Document o2) {
 				if(o1.getPageRank() - o2.getPageRank() == 0) {
@@ -26,7 +40,7 @@ public class Spearman {
             }
 		});
 		
-		return 0;
+		return sortedList;
 		
 	}
 
@@ -34,9 +48,10 @@ public class Spearman {
 	public static Object loadnumview(String numviewpath) throws IOException {
 		 FileInputStream fis = new FileInputStream(numviewpath);
 	        ObjectInputStream reader = new ObjectInputStream(fis);
-	        Map<String, Integer> new_numview=null;
+	        Map<String, Document> new_numview=null;
 			try {
-				new_numview = (Map<String, Integer>) reader.readObject();
+				//new_numview = (Map<String, Document>) reader.readObject();
+				reader.readObject();
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
@@ -49,7 +64,8 @@ public class Spearman {
 	        ObjectInputStream reader = new ObjectInputStream(fis);
 	        Map<String, Document> new_pagerank=null;
 			try {
-				new_pagerank = (Map<String, Document>) reader.readObject();
+				//new_pagerank = (Map<String, Document>) reader.readObject();
+				reader.readObject();
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
@@ -60,13 +76,20 @@ public class Spearman {
 	
 	
 	public static void main(String[] args) throws IOException{
-		if(args.length!=2){
+		loadnumview("/Users/soo/Desktop/Web_Search_Engine/data/index/numView.dat");
+	/*	if(args.length!=2){
 			System.out.println("Please provide 2 parameters");
 		}
 		else{
-			Map<String, Integer> numview=(Map<String, Integer>) loadnumview(args[1]);
-			Map<String, Document> pagerank=(Map<String, Document>) loadpagerank(args[0]);
-			Integer result=calculate(numview,pagerank);
-		}
+			Map<String, Document> numview=(Map<String, Document>) loadnumview(args[1]);
+			Map<String, Document> pagerank=(Map<String, Document>) loadpagerank(args[0]);*/
+		//Map<String, Document> numview=(Map<String, Document>) loadnumview("data/index/numView.dat");
+		//Map<String, Document> pagerank=(Map<String, Document>) loadpagerank("data/index/pagRank.dat");
+			//List<Document> sortednumview =sorting(numview);
+			//List<Document> sortedpagerank =sorting(pagerank);
+			//System.out.println(sortedpagerank);
+		//	Float result=calculate(sortednumview,sortedpagerank);
+		//	System.out.println(result);
+	//	}
 	}
 }
