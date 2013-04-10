ce350e2b63e43d950976f2ceaf063d628c4fd51e
oveis <jinil@nyu.edu>
2013-04-10 15:07:23 -0400

    Fix Spear calculation error and refactoring

--- src/edu/nyu/cs/cs2580/Spearman.java
+++ src/edu/nyu/cs/cs2580/Spearman.java
@@ -12,39 +12,72 @@ import java.util.Map;
 
 public class Spearman {
 
-    public static Float calculate(List<Document> sortednumview, List<Document> sortedpagerank) {
-        float result = 0;
-        float temp = 0;
-        for(int idx = 0;idx<sortednumview.size();idx++) {
-        	String doc = sortednumview.get(idx).getTitle();
-            temp = temp + (float) Math.pow(sortedpagerank.indexOf(doc) - sortednumview.indexOf(doc), 2.0d);
+    /**
+     * Calculation of Spearman's rank correlation coefficient
+     * @param sortednumview
+     * @param sortedpagerank
+     * @return coefficient
+     */
+    public float calculate(List<Document> sortednumview, List<Document> sortedpagerank) {
+        float temp = 0.0f;
+        float numDocs = (float)sortednumview.size();
+        
+        for(int idx = 0; idx < numDocs; idx++){
+            Document doc = sortednumview.get(idx);
+            int nvIndex = idx;
+            int pgIndex = sortedpagerank.indexOf(doc);
+
+            if(pgIndex == -1) continue;
+            temp += (float) Math.pow(pgIndex - nvIndex, 2.0d);
         }
-        result = (float) (1 - 6 * temp / (sortednumview.size() * (Math.pow(sortednumview.size(),2.0d) - 1)));
-        return result;
+        return (float) (1.0f - 6.0f * temp / (numDocs * ((float)Math.pow(numDocs, 2.0d) - 1.0f)));
     }
 
-    public static List<Document> sorting(Map<String, Document> temp) {
+    /**
+     * Sorting List<Document> with appropriate values 
+     * Mode 0 : PageRank List
+     * Mode 1 : NumView List
+     * @param temp
+     * @param mode
+     * @return sorted List
+     */
+    public List<Document> sorting(Map<String, Document> temp, int mode) {
         List<Document> sortedList = new LinkedList<Document>(temp.values());
-        Collections.sort(sortedList, new Comparator<Document>() {
-            @Override
-            public int compare(Document o1, Document o2) {
-                if (o1.getPageRank() - o2.getPageRank() == 0) {
-                    return o1.getTitle().compareTo(o2.getTitle());
-                } else {
-                    if (o1.getPageRank() - o2.getPageRank() < 0)
-                        return 1;
-                    else
-                        return -1;
+        if(mode == 0){  // sorting with Page Rank Value
+            Collections.sort(sortedList, new Comparator<Document>() {
+                @Override
+                public int compare(Document o1, Document o2) {
+                    if (o1.getPageRank() - o2.getPageRank() == 0) {
+                        return o1.getTitle().compareTo(o2.getTitle());  
+                    } else {
+                        return (o1.getPageRank() - o2.getPageRank() < 0) ? 1 : -1;
+                    }
                 }
-            }
-        });
-
+            });
+        }else if(mode == 1){    // sorting with Num View
+            Collections.sort(sortedList, new Comparator<Document>() {
+                @Override
+                public int compare(Document o1, Document o2) {
+                    if (o1.getNumViews() - o2.getNumViews() == 0) {
+                        return o1.getTitle().compareTo(o2.getTitle());  
+                    } else {
+                        return (o1.getNumViews() - o2.getNumViews() < 0) ? 1 : -1;
+                    }
+                }
+            });
+        }
         return sortedList;
-
     }
 
+    /**
+     * Load Map<String, Document> from files and build Sorted List
+     * @param path
+     * @param mode
+     * @return sorted List
+     * @throws IOException
+     */
     @SuppressWarnings("unchecked")
-    public static Map<String, Document> load(String path) throws IOException {
+    public List<Document> load(String path, int mode) throws IOException {
         FileInputStream fis = new FileInputStream(path);
         ObjectInputStream reader = new ObjectInputStream(new BufferedInputStream(fis));
         Map<String, Document> new_data = null;
@@ -54,20 +87,21 @@ public class Spearman {
             e.printStackTrace();
         }
         reader.close();
-        return new_data;
-
+        return sorting(new_data, mode);
     }
 
     public static void main(String[] args) throws IOException {
-         if(args.length!=2){
-        	 System.out.println("Please provide 2 parameters"); } 
-         else{
-	         Map<String, Document> numview = load(args[1]); 
-	         Map<String, Document> pagerank = load(args[0]);
+        Spearman spearman = new Spearman();
+        
+        if(args.length!=2){
+        	 System.out.println("Please provide 2 parameters"); 
+        	 System.out.println("FORMAT : java edu.nyu.cs.cs2580.Spearman <PATH-TO-PAGERANKS> <PATH-TO-NUMVIEWS>");
+       } else{
+	         List<Document> sortednumview, sortedpagerank;
+	         sortedpagerank = spearman.load(args[0], 0);
+	         sortednumview = spearman.load(args[1], 1);
 	         
-	         List<Document> sortednumview =sorting(numview);
-	         List<Document> sortedpagerank =sorting(pagerank);
-	         Float result=calculate(sortednumview,sortedpagerank);
+	         Float result = spearman.calculate(sortednumview,sortedpagerank);
 	         System.out.println(result);
          }
     }
